90f86d4795e526588788432121670fd47dca0706
kuzmavasil74 <98240149+kuzmavasil74@users.noreply.github.com>
2024-02-24 16:18:36 +0100

    update task 6

--- js/task-6.js
+++ js/task-6.js
@@ -4,41 +4,35 @@ function getRandomHexColor() {
     .padStart(6, '0')}`;
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('#controls input');
-  const createBtn = document.querySelector('[data-create]');
-  const destroyBtn = document.querySelector('[data-destroy]');
-  const boxesContainer = document.querySelector('#boxes');
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
 
-  createBtn.addEventListener('click', createBoxes);
-  destroyBtn.addEventListener('click', destroyBoxes);
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
 
-  function createBoxes() {
-    const amount = input.valueAsNumber;
-    if (amount >= 1 && amount <= 100) {
-      const boxes = [];
-      let size = 30;
-
-      for (let i = 0; i < amount; i++) {
-        const box = document.createElement('div');
-        box.style.backgroundColor = getRandomHexColor();
-        box.style.width = `${size}px`;
-        box.style.height = `${size}px`;
-        box.style.marginBottom = '10px';
-        boxes.push(box);
-        size += 10;
-      }
-
-      boxesContainer.innerHTML = '';
-      boxesContainer.append(...boxes);
-    } else {
-      alert('Будь ласка, введіть число від 1 до 100.');
+function createBoxes() {
+  const amount = input.valueAsNumber;
+  if (amount > 0 && amount <= 100) {
+    const boxes = [];
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+      const box = document.createElement('div');
+      box.style.backgroundColor = getRandomHexColor();
+      box.style.width = `${size}px`;
+      box.style.height = `${size}px`;
+      boxes.push(box);
+      size += 10;
     }
-
-    input.value = '';
-  }
-
-  function destroyBoxes() {
     boxesContainer.innerHTML = '';
+    boxesContainer.append(...boxes);
+  } else {
+    alert(`Будь ласка, введіть число від 1 до 100.`);
   }
-});
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
